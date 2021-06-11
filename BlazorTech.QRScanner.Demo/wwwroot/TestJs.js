17fb25c5d89e4fac2abe38ff96543842533fcd56
Kazbek <kazbek.se@gmail.com>
2021-06-11 15:58:36 +0300

    Добавьте файлы проекта.

---
+++ BlazorTech.QRScanner.Demo/wwwroot/TestJs.js
@@ -0,0 +1,8 @@
+﻿function qeReaderTest(videoElementId,contentElementId) {
+    var videoElem = document.getElementById(videoElementId)
+
+    var contentElem = document.getElementById(contentElementId);
+    const qrScanner = new QrScanner(videoElem, result => { console.log('decoded qr code:', result); contentElem.innerHTML = result });
+
+    qrScanner.start();
+}
