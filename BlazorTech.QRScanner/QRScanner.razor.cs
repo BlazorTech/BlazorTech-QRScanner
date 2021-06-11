fd647480d3d7048f55303a7f1a2444d12d924cfd
Kazbek <kazbek.se@gmail.com>
2021-06-12 00:45:02 +0300

    Non unique timeout.

--- BlazorTech.QRScanner/QRScanner.razor.cs
+++ BlazorTech.QRScanner/QRScanner.razor.cs
@@ -1,28 +1,29 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BlazorTech.QRScanner
 {
-    public partial class QRScanner
+    public partial class QRScanner : IDisposable
     {
         [Inject] protected IJSRuntime jSRuntime { get; set; }
 
         [Parameter] public bool AutoStart { get; set; } = true;
+        [Parameter] public int NonUniqueTimeout { get; set; } = 0;
         [Parameter] public EventCallback<string> OnScanReceived { get; set; }
         
         
         private IJSObjectReference _qrScanner { get; set; }
-
-        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
         private ElementReference _videoElement { get; set; }
+        
+        
+        private readonly DotNetObjectReference<QRScannerCallbackListner> _qrScannerCallbackListnerRef;
+        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
 
         public QRScanner()
         {
+            _qrScannerCallbackListnerRef = DotNetObjectReference.Create(new QRScannerCallbackListner(this));
             moduleTask = new(() => jSRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/BlazorTech.QRScanner/qr-scanner.umd.min.js").AsTask());
         }
 
@@ -30,9 +31,8 @@ namespace BlazorTech.QRScanner
         {
             if (firstRender)
             {
-                DotNetObjectReference<QRScanner> objRef = DotNetObjectReference.Create(this);
                 var popperWrapper = await moduleTask.Value;
-                _qrScanner = await popperWrapper.InvokeAsync<IJSObjectReference>("createQrScanner", _videoElement, objRef);
+                _qrScanner = await popperWrapper.InvokeAsync<IJSObjectReference>("createQrScanner", _videoElement, _qrScannerCallbackListnerRef, NonUniqueTimeout);
                 if(AutoStart)
                     await StartCapturing();
             }
@@ -48,16 +48,23 @@ namespace BlazorTech.QRScanner
             await _qrScanner.InvokeVoidAsync("stop");
         }
 
-        [JSInvokable("SendCodeAsync")]
-        public async Task SendCodeAsync(string qr)
+        public void Dispose()
         {
-            Console.WriteLine($"Scanned:{qr}");
-            await OnScanReceived.InvokeAsync(qr);
+            _qrScannerCallbackListnerRef.Dispose();
         }
 
-        public class QRScannerCallbackListner
+        protected class QRScannerCallbackListner
         {
-            
+            protected readonly QRScanner _qrScanner;
+            public QRScannerCallbackListner(QRScanner qRScanner)
+            {
+                _qrScanner = qRScanner;
+            }
+            [JSInvokable("SendCodeAsync")]
+            public async Task SendCodeAsync(string qr)
+            {
+                await _qrScanner.OnScanReceived.InvokeAsync(qr);
+            }
         }
         
     }
