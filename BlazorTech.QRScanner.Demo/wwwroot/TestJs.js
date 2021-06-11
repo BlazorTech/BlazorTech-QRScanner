00ff06293d8e6462a068f41625039085aca6f291
Kazbek <kazbek.se@gmail.com>
2021-06-11 22:58:49 +0300

    Test

--- BlazorTech.QRScanner.Demo/wwwroot/TestJs.js
+++ BlazorTech.QRScanner.Demo/wwwroot/TestJs.js
@@ -5,4 +5,14 @@
     const qrScanner = new QrScanner(videoElem, result => { console.log('decoded qr code:', result); contentElem.innerHTML = result });
 
     qrScanner.start();
+
+    return qrScanner;
+}
+
+function createQrScanner(videoElementId, contentElementId) {
+    var videoElem = document.getElementById(videoElementId)
+    var contentElem = document.getElementById(contentElementId);
+    const qrScanner = new QrScanner(videoElem, result => { console.log('decoded qr code:', result); contentElem.innerHTML = result });
+
+    return qrScanner;
 }
